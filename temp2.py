3f9389c22d033fa60691a88b0a7a97bfda820fab
Shaibal Karmakar <shaibal.karmakar@pokemon.icts.res.in>
2024-09-11 18:03:42 +0530

    update

--- temp2.py
+++ temp2.py
@@ -1,13 +1,13 @@
 import os
 import pickle
 
-path = os.path.join(os.getcwd(), "test4")
+path = os.path.join(os.getcwd(), "test5")
 files = os.listdir(path)
 
 done = 0
 for file_ in files:
     with open(os.path.join(path, file_), "rb") as fp:
         a = dict(pickle.load(fp))
-        if a["p"] == 0.24:
+        if a["p"] == 0.2475:
             done += a["trials_done"]
 print(done)
